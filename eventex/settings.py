6a3ed67fd285e9e496b4b3d3488268fd4ac80520
ricardoanzolin <ricardoanzolin@gmail.com>
2015-12-28 22:17:13 -0200

    change

--- eventex/settings.py
+++ eventex/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
@@ -119,8 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles/')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'static/'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
