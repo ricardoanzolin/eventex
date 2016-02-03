ad1b7b514eefe9a25169221a17e9c771852edb46
ricardoanzolin <ricardoanzolin@gmail.com>
2016-02-02 22:12:11 -0200

    Email ou telefone opcionais

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -35,6 +35,21 @@ class SubscriptionFormTest(TestCase):
         form = self.make_validated_form(name='RICARDO anzolin')
         self.assertEqual('Ricardo Anzolin', form.cleaned_data['name'])
 
+    def test_email_is_optional(self):
+        """Email is optional"""
+        form = self.make_validated_form(email='')
+        self.assertFalse(form.errors)
+
+    def test_phone_is_optional(self):
+        """Phone is optional"""
+        form = self.make_validated_form(phone='')
+        self.assertFalse(form.errors)
+
+    def test_must_inform_email_or_phone(self):
+        """Email and phone are optional, but one must be informed."""
+        form = self.make_validated_form(email='', phone='')
+        self.assertListEqual(['__all__'], list(form.errors))
+
     def make_validated_form(self, **kwargs):
         valid = dict(name='Ricardo Anzolin', cpf='12345678901', email='ricardoanzolin@gmail.com',phone='46-84848484')
         data = dict(valid, **kwargs)
