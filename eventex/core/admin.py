c4d94b6b5463cb3d20bb8eedafee456836fc7ee9
ricardoanzolin <ricardoanzolin@gmail.com>
2016-02-22 22:03:16 -0300

    Talk

--- eventex/core/admin.py
+++ eventex/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from eventex.core.models import Speaker, Contact
+from eventex.core.models import Speaker, Contact, Talk
 
 # Register your models here.
 
@@ -25,3 +25,4 @@ class SpeakerModelAdmin(admin.ModelAdmin):
     photo_img.short_description = 'foto'
 
 admin.site.register(Speaker, SpeakerModelAdmin)
+admin.site.register(Talk)
